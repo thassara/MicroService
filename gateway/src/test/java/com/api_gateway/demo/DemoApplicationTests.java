f248ec455be731353bc8a93d7cce5c59c1127b0a
madush <137921529+thassara@users.noreply.github.com>
2025-04-16 19:41:57 +0530

    api gate way chages add others

---
+++ gateway/src/test/java/com/api_gateway/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_gateway.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
