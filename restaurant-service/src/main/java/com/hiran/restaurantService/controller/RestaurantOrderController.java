25ab7b120e95124793f6ce77b24277dec2a7d2cb
Hiran Rathnayake <128971275+Hiranx@users.noreply.github.com>
2025-04-28 13:16:19 +0530

    order controlleer changed and created a optional corscofig file for testing

--- restaurant-service/src/main/java/com/hiran/restaurantService/controller/RestaurantOrderController.java
+++ restaurant-service/src/main/java/com/hiran/restaurantService/controller/RestaurantOrderController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/restaurants/{restaurantId}/orders")
+@RequestMapping("/api/orders/restaurant/{restaurantId}/orders")
 public class RestaurantOrderController {
 
     private final RestaurantOrderService restaurantOrderService;
